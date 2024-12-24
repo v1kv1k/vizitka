f5fbf0670857deb524052349d2ce19abaa339fb0
v1kv1k <viktorgerasimenkome21@gmail.com>
2024-12-24 22:03:26 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+            }
+        });
+    });
+    document.querySelectorAll('.skill-card').forEach((card) => {
+        observer.observe(card);
+    });
+});
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+}); 
